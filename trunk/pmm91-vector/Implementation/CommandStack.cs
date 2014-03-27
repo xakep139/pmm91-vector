a89034ad8d4689ddc4c39ffdd4aecd6c665e94fb
xakep139@gmail.com <xakep139@gmail.com@773eec5d-b9d9-e619-c59c-be9de0b994e0>
2014-03-27 02:02:20 +0000

    Стек команд теперь хранится именно стеком; добавил комментарии (в т.ч. для бригады IO & com);

--- trunk/pmm91-vector/Implementation/CommandStack.cs
+++ trunk/pmm91-vector/Implementation/CommandStack.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pmm91_vector.Implementation
 {
+    /// <summary>
+    /// Класс реализующий интерфейс стека команд
+    /// </summary>
     public class CommandStack : Interfaces.ICommandStack
     {
-        private int _curCommand = -1;
-        private List<Interfaces.ICommand> _stack = new List<Interfaces.ICommand>();
+        /// <summary>
+        /// Стек выполненных команд
+        /// </summary>
+        private Stack<Interfaces.ICommand> _stackUndo = new Stack<Interfaces.ICommand>();
+        
+        /// <summary>
+        /// Стек отменённых команд
+        /// </summary>
+        private Stack<Interfaces.ICommand> _stackRedo = new Stack<Interfaces.ICommand>();
         private static CommandStack instance = null;
 
         /// <summary>
@@ -35,58 +42,66 @@ namespace pmm91_vector.Implementation
 
         void Interfaces.ICommandStack.DoComand(Interfaces.ICommand command)
         {
+            ////
+            ////    Подумать насчёт того, следует ли выполнять команду здесь
+            ////
             throw new NotImplementedException();
-            this._curCommand++;
-            if (this._curCommand < this._stack.Count)
-                this._stack.RemoveRange(this._curCommand, this._stack.Count - this._curCommand);
-            this._stack.Add(command);
+            this._stackRedo.Clear();        //Стек отменённых команд очищаем
+            this._stackUndo.Push(command);  //Добавляем выполняемую команду в основной стек
         }
 
         void Interfaces.ICommandStack.UndoComand()
         {
+            ////
+            ////    Подумать насчёт того, как сделать так, чтобы отменить изменения, сделанные командой
+            ////
             throw new NotImplementedException();
-            this._curCommand--;
+            this._stackRedo.Push(this._stackUndo.Pop());
         }
 
         void Interfaces.ICommandStack.RedoComand()
         {
-            throw new NotImplementedException();
-            this._curCommand++;
+            ////
+            ////    Подумать насчёт того, где хранить параметры команды (сейчас задал как null)
+            ////
+            this._stackUndo.Push(this._stackRedo.Pop());
+            this._stackUndo.Peek().Execute(null);
         }
 
         bool Interfaces.ICommandStack.CanUndo()
         {
-            return (this._curCommand > 0);
+            return (this._stackUndo.Count > 0);
         }
 
         bool Interfaces.ICommandStack.CanRedo()
         {
-            return (this._curCommand < this._stack.Count - 1);
+            return (this._stackRedo.Count > 0);
         }
 
         void ICollection<Interfaces.ICommand>.Add(Interfaces.ICommand item)
         {
-            this._stack.Add(item);
+            this._stackUndo.Push(item);
         }
 
         void ICollection<Interfaces.ICommand>.Clear()
         {
-            this._stack.Clear();
+            this._stackRedo.Clear();
+            this._stackUndo.Clear();
         }
 
         bool ICollection<Interfaces.ICommand>.Contains(Interfaces.ICommand item)
         {
-            return this._stack.Contains(item);
+            return this._stackUndo.Contains(item);
         }
 
         void ICollection<Interfaces.ICommand>.CopyTo(Interfaces.ICommand[] array, int arrayIndex)
         {
-            this._stack.CopyTo(array, arrayIndex);
+            this._stackUndo.CopyTo(array, arrayIndex);
         }
 
         int ICollection<Interfaces.ICommand>.Count
         {
-            get { return this._stack.Count; }
+            get { return this._stackUndo.Count; }
         }
 
         bool ICollection<Interfaces.ICommand>.IsReadOnly
@@ -96,17 +111,20 @@ namespace pmm91_vector.Implementation
 
         bool ICollection<Interfaces.ICommand>.Remove(Interfaces.ICommand item)
         {
-            return this._stack.Remove(item);
+            var tmp = this._stackUndo.ToList();
+            bool isRemoved = tmp.Remove(item);
+            this._stackUndo = new Stack<Interfaces.ICommand>(tmp);
+            return isRemoved;
         }
 
         IEnumerator<Interfaces.ICommand> IEnumerable<Interfaces.ICommand>.GetEnumerator()
         {
-            return this._stack.GetEnumerator();
+            return this._stackUndo.GetEnumerator();
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
-            return this._stack.GetEnumerator();
+            return this._stackUndo.GetEnumerator();
         }
     }
 }
