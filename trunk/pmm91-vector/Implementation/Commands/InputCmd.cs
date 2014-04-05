07657616f66f62d777735a07f304244516f1d280
voron.sp@gmail.com <voron.sp@gmail.com@773eec5d-b9d9-e619-c59c-be9de0b994e0>
2014-04-05 16:53:22 +0000

    Добавлены болванки команд ввода/вывода

---
+++ trunk/pmm91-vector/Implementation/Commands/InputCmd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace pmm91_vector.Implementation.Commands
+{
+    public class InputCmd:Interfaces.ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt"; //например, txt
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            var result = openFileDialog.ShowDialog();
+            if (result.HasValue && result.Value == true)
+            {
+                string path = openFileDialog.FileName;
+                StreamReader sr = new StreamReader(path);
+                //тут вызываем метод bool Load(Stream fileStream)
+            }
+        }
+    }
+}
---
+++ trunk/pmm91-vector/Implementation/Commands/OutputCmd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace pmm91_vector.Implementation.Commands
+{
+    class OutputCmd : Interfaces.ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = "c:\\";
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt"; //например, txt
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            var result = saveFileDialog.ShowDialog();
+            if (result.HasValue && result.Value == true)
+            {
+                string path = saveFileDialog.FileName;
+                StreamReader sw = new StreamReader(path);
+                //тут вызываем метод bool Save(Stream fileStream)
+            }
+        }
+    }
+}
