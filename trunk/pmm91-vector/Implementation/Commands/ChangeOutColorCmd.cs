8a680187b7df718e4803ca2088a7ad373cba8ec8
xakep139@gmail.com <xakep139@gmail.com@773eec5d-b9d9-e619-c59c-be9de0b994e0>
2014-04-11 04:08:11 +0000

    Добавил иконок (цвет менять в Icons.xaml); Добавил кнопок на меню и привязал к ним команды (пока пустые); Иконки верхнего меню для разнообразия растровые; Добавил разделители в нижнем меню.

---
+++ trunk/pmm91-vector/Implementation/Commands/ChangeOutColorCmd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pmm91_vector.Implementation.Commands
+{
+    public class ChangeOutColorCmd : Interfaces.ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            //TODO: сделать команду активной, только если выбрана некоторая фигура
+            return true;
+        }
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
