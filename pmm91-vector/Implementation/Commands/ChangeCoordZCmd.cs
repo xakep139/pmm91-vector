1d2a6622008a18f4f910b2e98dc0170cc72957fe
xakep139@gmail.com <xakep139@gmail.com@773eec5d-b9d9-e619-c59c-be9de0b994e0>
2014-04-11 04:08:11 +0000

    Добавил иконок (цвет менять в Icons.xaml); Добавил кнопок на меню и привязал к ним команды (пока пустые); Иконки верхнего меню для разнообразия растровые; Добавил разделители в нижнем меню.

---
+++ pmm91-vector/Implementation/Commands/ChangeCoordZCmd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pmm91_vector.Implementation.Commands
+{
+    class ChangeCoordZCmd : Interfaces.ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            //TODO: сделать команду активной, только если выбрана некоторая фигура
+            return true;
+        }
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+
+        public void Execute(object parameter)
+        {
+            CommandStack.Instance.DoCommand(this);
+            string direction = (parameter as string).ToLower();
+            switch (direction)
+            {
+                //TODO: написать реальное изменение Z-координаты
+                case "up":
+                    break;
+                case "down":
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
